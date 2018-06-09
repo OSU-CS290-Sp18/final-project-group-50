56d64755ab0c716aaa68fe030364a559aef214e6
ChasenYamashita <32147061+ChasenYamashita@users.noreply.github.com>
2018-06-08 17:43:14 -0700

    404 and server.js functions

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-const publicPath = path.join(__dirname, './public');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,14 +9,25 @@ var port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
+    console.log('reached 404');
       res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.get('/', function (req, res) {
     console.log('Going to index');
-    res.status(200).render('pracIndex');
+    res.sendFile('/pracIndex.html');
 
 });
+/*
+
+app.get('/snacks', function (req, res) {
+    res.sendFile('/snacks.html');
+});
+
+app.get('/sitemap', function (req, res) {
+    res.sendFile('/drinks.html');
+});
+*/
 
 app.listen(port, function () {
       console.log("== Server is listening on port", port);
