fee70a509bc5f492f5bd146bb1da9ed29b567229
ChasenYamashita <32147061+ChasenYamashita@users.noreply.github.com>
2018-06-09 19:17:34 -0700

    added mongo on server.js

--- server.js
+++ server.js
@@ -2,11 +2,62 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoDB = null;
+
+//var mongoURL =
+//	'mongodb://' + mongoUsername + ':' + mongoPassword + '@' +
+//	mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoURL = 'mongodb://localhost:27017';
+
+MongoClient.connect(mongoURL, function (err, client) {
+    if (err) {
+        throw err;
+    }
+    db = mongoDBDatabase = client.db(mongoDBName);
+    app.listen(3000, function () {
+        console.log("== Server listening on port 3000");
+    });
+});
+
+/*
+
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    client.close();
+});
+
+*/
+
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
 
 app.get('*', function (req, res) {
     console.log('reached 404');
@@ -27,8 +78,9 @@ app.get('/snacks', function (req, res) {
 app.get('/sitemap', function (req, res) {
     res.sendFile('/drinks.html');
 });
-*/
+
 
 app.listen(port, function () {
       console.log("== Server is listening on port", port);
 });
+*/
