0eb769855d777e3b333a4fb6c4987abf6b877dab
ChasenYamashita <32147061+ChasenYamashita@users.noreply.github.com>
2018-06-13 00:33:43 -0700

    Ragnarok

--- public/index.js
+++ public/index.js
@@ -1,33 +1,13 @@
-/*
-
-var slideIndex = 0;
-
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-*/
-
-
 var cart = document.getElementById('shopping-cart-button');
+var modal = document.getElementById('create-twit-modal');
 
-cart.addEventListener('click', openModal);
 
-function openModal() {
-    console.log("button pressed");
+cart.addEventListener('click',openModal);
+
+function openModal()
+{
+console.log("button pressed");
+modal.style.display = 'block';
+modal.style.backgroundColor = 'rgba(0,0,0,0.85)';
 
 }
